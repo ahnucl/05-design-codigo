449b9a10f642518749d92f7122155bf6b952f86e
Leonardo Cunha <leonardo.cunha@comunix.net.br>
2024-12-09 13:01:24 +0000

    Teste caminho de erro

--- src/calculators/calculator_1_test.py
+++ src/calculators/calculator_1_test.py
@@ -11,11 +11,14 @@ mock: elemento controlado para trabalho
 """
 
 from typing import Dict
+from pytest import raises
 from calculator_1 import Calculator1
+
 
 class MockRequest:
     def __init__(self, body: Dict) -> None:
         self.json = body
+
 
 def test_calculate():
     mock_request = MockRequest(body={"number": 1})
@@ -31,3 +34,13 @@ def test_calculate():
     # Assertividade da resposta
     assert response['data']['result'] == 14.25
     assert response['data']['Calculator'] == 1
+    
+    
+def test_calculate_with_body_error():
+    mock_request = MockRequest(body={"something": 1})
+    calculator_1 = Calculator1()
+
+    with raises(Exception) as excinfo:
+        calculator_1.calculate(mock_request)
+
+    assert str(excinfo.value) == "body mal formatado!"
