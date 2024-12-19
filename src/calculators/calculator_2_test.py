7fe749c9378c1705cd9c7d3bb833ce7093bd9553
Leonardo Cunha <leonardo.cunha@comunix.net.br>
2024-12-19 13:55:19 +0000

    Cria mock para handler

--- src/calculators/calculator_2_test.py
+++ src/calculators/calculator_2_test.py
@@ -1,6 +1,7 @@
-from typing import Dict
+from typing import Dict, List
 from .calculator_2 import Calculator2
 from src.drivers.numpy_handler import NumpyHandler
+from src.drivers.interfaces.driver_handler_interface import DriverHandlerInterface
 
 
 class MockRequest:
@@ -8,13 +9,29 @@ class MockRequest:
         self.json = body
 
 
-def test_calculate():
+class MockDriverHandler(DriverHandlerInterface):
+    def standard_deviation(self, data: List[float]) -> float:
+        return 12.5
+
+
+# Integração entre NumpyHandler e Calculator2
+def test_calculate_integration():
     mock_request = MockRequest({"numbers": [2.12, 4.62, 1.32]})
-    
+
     driver = NumpyHandler()
     calculator_2 = Calculator2(driver)
     formated_response = calculator_2.calculate(mock_request)
-    
+
     assert isinstance(formated_response, dict)
-    assert formated_response == {'data': {'Calculator': 2, 'result': 0.08}}
-    
+    assert formated_response == {"data": {"Calculator": 2, "result": 0.08}}
+
+
+def test_calculate():
+    mock_request = MockRequest({"numbers": [2.12, 4.62, 1.32]})
+
+    driver = MockDriverHandler()
+    calculator_2 = Calculator2(driver)
+    formated_response = calculator_2.calculate(mock_request)
+
+    assert isinstance(formated_response, dict)
+    assert formated_response == {"data": {"Calculator": 2, "result": 0.08}}
