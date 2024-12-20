3dadeb115afc4dd30b0fe49fa8b1fdf321b5594b
Leonardo Cunha <leonardo.cunha@comunix.net.br>
2024-12-20 15:19:31 +0000

    Finaliza testes da calc3

--- src/calculators/calculator_3_test.py
+++ src/calculators/calculator_3_test.py
@@ -1,7 +1,7 @@
 from typing import Dict, List
+from pytest import raises
 from .calculator_3 import Calculator3
 from src.drivers.numpy_handler import NumpyHandler
-from src.drivers.interfaces.driver_handler_interface import DriverHandlerInterface
 
 
 class MockRequest:
@@ -9,32 +9,32 @@ class MockRequest:
         self.json = body
 
 
-class MockDriverHandler(DriverHandlerInterface):
-    def standard_deviation(self, numbers):
-        return super().standard_deviation(numbers)
-    
+class MockDriverHandlerError:
     def variance(self, data: List[float]) -> float:
-        return 20
+        return 3
 
 
-# Integração entre NumpyHandler e Calculator2
-def test_calculate_integration():
-    mock_request = MockRequest({"numbers": [2.12, 4.62, 1.32]})
-
-    driver = NumpyHandler()
-    calculator_3 = Calculator3(driver)
-    formated_response = calculator_3.calculate(mock_request)
-
-    assert isinstance(formated_response, dict)
-    assert formated_response == {"data": {"Calculator": 3, "result": 'sucesso'}}
+class MockDriverHandler:
+    def variance(self, data: List[float]) -> float:
+        return 1000000
 
 
-def test_calculate():
-    mock_request = MockRequest({"numbers": [2.12, 4.62, 1.32]})
+# Integração entre NumpyHandler e Calculator3
+def test_calculate_integration_with_variance_lower_than_multiplication():
+    mock_request = MockRequest({"numbers": [1, 2, 3, 4, 5]})
+    calculator_3 = Calculator3(MockDriverHandlerError())
 
-    driver = MockDriverHandler()
-    calculator_3 = Calculator3(driver)
-    formated_response = calculator_3.calculate(mock_request)
+    with raises(Exception) as excinfo:
+        calculator_3.calculate(mock_request)
 
-    assert isinstance(formated_response, dict)
-    assert formated_response == {"data": {"Calculator": 3, "result": 'falha'}}
+    assert str(excinfo.value) == 'Falha no processo: Variância menor que multiplicação'
+
+
+def test_calculate_integration_with_variance_higher_than_multiplication():
+    mock_request = MockRequest({"numbers": [1, 1, 1, 1, 100]})
+    calculator_3 = Calculator3(MockDriverHandler())
+
+    response = calculator_3.calculate(mock_request)
+
+    assert isinstance(response, dict)
+    assert response == {'data': {'Calculator': 3, 'value': 1000000, 'success': True}}
